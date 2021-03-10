6504d372971c0d6fa4c68f26457dc8b3b5b1b9b0
Your Name <hectorfrramirez@gmail.com>
2021-03-10 18:06:56 -0500

    test

--- src/components/CoachApplication/index.js
+++ src/components/CoachApplication/index.js
@@ -11,9 +11,9 @@ const CoachApplication = () => {
         <div id= "CoachAppPage">
             <div  id = "headerMessage" style = {{paddingTop:"50px", height: "50px"}}className = "col-sm-5 mx-auto">
                 <h1>Evolve and Start your
-                    <br/> Journey as an Evolve U Coach!</h1>
+                    <br/> Journey as an Evolve U Coach</h1>
             </div>
-            
+
             {area} 
         </div>
     )
